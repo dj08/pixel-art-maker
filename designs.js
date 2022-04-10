c2bbe0efa5ab78e03b7955195074a4d6d6e0d523
Dushyant Juneja <juneja.dushyant@gmail.com>
2022-04-10 11:33:14 -0400

    First cut app

--- designs.js
+++ designs.js
@@ -1,10 +1,55 @@
-// Select color input
-// Select size input
+// Attaching to start of event to avoid trouble loading functions
+document.addEventListener("DOMContentLoaded", () => {
+    // Get grid parameters -- h, w, color
+    const gridParameters = getGridParameters();
 
-// When size is submitted by the user, call makeGrid()
+    // Create initial pixel grid based using the values
+    createPixelGrid(gridParameters.height, gridParameters.width);
 
-function makeGrid() {
+    // Add event listeners on the submit button to update
+    // grid as needed
+    const submitBtn = document.querySelector("#sizePicker input[type=submit]");
+    submitBtn.addEventListener("click", e => {
+        e.preventDefault();
+        const params = getGridParameters();
+        createPixelGrid(params.height, params.width);
+    })
 
-// Your code goes here!
+    // Add event listener on canvas to update cell color
+    document.querySelector("#pixelCanvas").addEventListener("click", e =>
+        e.target.style.backgroundColor = gridParameters.pickedColor);
 
+    // Add event listener on color picker to update cell view
+    document.querySelector("#colorPicker").addEventListener("input", e => {
+        gridParameters.pickedColor = e.target.value;
+        console.log("Updated color!");
+    });
+
+});
+
+// Function to get parameter values for the grid
+const getGridParameters = () =>
+    ({
+        height: document.querySelector("#inputHeight").value,
+        width: document.querySelector("#inputWidth").value,
+        pickedColor: document.querySelector("#colorPicker").value
+    });
+
+// Main function to create the pixel grid
+const createPixelGrid = (height, width) => {
+    const pixelCanvas = document.querySelector("#pixelCanvas");
+
+    // Refresh canvas
+    pixelCanvas.innerHTML = "";
+
+    // Rebuild pixel grid
+    for (let x = 0; x < height; x++) {
+        let pixelColumn = `<tr class="pixel-row" id="${"r"+x}">`;
+
+        for (let y = 0; y < width; y++) {
+            pixelColumn += `<td class="pixel-cell" id="${"r"+x+"-c"+y}"></td>`;
+        }
+
+        document.querySelector("#pixelCanvas").innerHTML += pixelColumn;
+    }
 }
